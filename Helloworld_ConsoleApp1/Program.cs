9183abb5413f38a24356c4d7ff0c30c62fc00825
jaruwankha-eng <jaruwan.kha@kkumail.com>
2025-12-02 10:28:24 +0700

    add code

---
+++ Helloworld_ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prinr "Hello World" to the console
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My name is Jaruwan Khaolueang");
+            Console.WriteLine("Student ID : 683450184-0");
+            Console.WriteLine("Major : Computer and Information Science");
+            Console.WriteLine("Faculty : IS");
+            Console.WriteLine("Khon Kaen University");
+        }
+    }
+}
